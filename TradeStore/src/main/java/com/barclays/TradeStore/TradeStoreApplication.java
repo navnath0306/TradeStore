215a135c82067dcdf327105c587ce52d154e72cf
NAVNATH KADAM <kadamnavnath20@gmail.com>
2022-04-10 11:23:30 +0530

    Created project structure

---
+++ TradeStore/src/main/java/com/barclays/TradeStore/TradeStoreApplication.java
@@ -0,0 +1,13 @@
+package com.barclays.TradeStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeStoreApplication.class, args);
+	}
+
+}
